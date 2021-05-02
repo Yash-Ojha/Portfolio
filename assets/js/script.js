ff1ea711696ee351c1e726caa463558419368e79
Yash Ojha <71380586+Yash-Ojha@users.noreply.github.com>
2021-05-02 20:51:09 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,34 @@ jQuery('.menu-btn').on('click', function () {
     jQuery('.menu-btn>i').toggleClass('fa-bars');
     jQuery('.menu-btn>i').toggleClass('fa-times');
 });
+
+  VanillaTilt.init(document.querySelectorAll(".services-card"), {
+    max: 25,
+    speed: 400,
+    gyroscope:false,
+  });
+ 
+  jQuery(function () {
+    jQuery('.services-card').matchHeight('byRow:false');
+  });
+ 
+ AOS.init({
+    offset: 150,
+    duration: 1200,
+    disable: 'mobile',
+    useClassNames: true,
+  });
+ 
+jQuery('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    margin: 10,
+    autoplay:true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive:{
+      0:{
+          items:1
+      }
+  }
+});
